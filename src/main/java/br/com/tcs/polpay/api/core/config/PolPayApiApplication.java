66908aef99a21bbf84bee40a53e5587b3fae3e71
Tadeu.CS <tcssistemas2012@gmail.com>
2022-02-07 21:52:30 -0300

    Criação de projeto e estrutura de pastas

---
+++ src/main/java/br/com/tcs/polpay/api/core/config/PolPayApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.tcs.polpay.api.core.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolPayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolPayApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/tcs/polpay/api/PolPayApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tcs.polpay.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolPayApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
